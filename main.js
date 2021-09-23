efec966a1b8313b9b4ebcb85395dc52db323f40b
salrey <tekdehn@gmail.com>
2021-09-23 13:18:51 -0400

    Set up to create new article and append

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const section1 = document.querySelector("section");
 section1.classList.add("featured");
+
+//--------Helper Function------
+
+const newElementWithText = (tag, text) => {
+  const element = document.createElement(tag);
+  element.textContent = text;
+  return element;
+};
+
+const newArticle = document.createElement("article");
+
+const postSection = document.querySelector(".posts");
+postSection.append(newArticle);
