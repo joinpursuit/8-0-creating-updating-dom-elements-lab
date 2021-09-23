c66b899e9239d9914e95ced9c0a7a10c93387934
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-23 13:35:30 -0400

    created empty article element to test placement at end of section.posts element

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 let featured = document.querySelector("main section");
 // console.log(featured);
 featured.setAttribute("class", "featured");
+
+// create `article` element with JavaScript.
+let article = document.createElement("article");
+article.textContent = "test";
+
+
+
+
+
+// add this `article` element to end of `sections.post` element
+let endOfSection = document.querySelector(".posts");
+endOfSection.append(article);
