c875d137dd7435efa63b83bc92a0ad553ddb3970
salrey <tekdehn@gmail.com>
2021-09-23 13:29:25 -0400

    Create and append img element

--- main.js
+++ main.js
@@ -11,5 +11,10 @@ const newElementWithText = (tag, text) => {
 
 const newArticle = document.createElement("article");
 
+const img = document.createElement("img");
+img.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+img.setAttribute("alt", "Image of a mountain in front of a lake.");
+newArticle.append(img);
+
 const postSection = document.querySelector(".posts");
 postSection.append(newArticle);
