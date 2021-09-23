e9a532fb65e32b3d963e4967ca3801084337620d
Christina Loiacono <loiaconochristina@gmail.com>
2021-09-23 13:55:07 -0400

    created paragraph and aside sections of article

--- main.js
+++ main.js
@@ -17,8 +17,34 @@ mountainHeader.textContent = "Stop Planning";
 article.append(mountainHeader);
 
 // create paragraph
+let mountainParagraph = document.createElement("p");
+mountainParagraph.textContent= "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?";
+article.append(mountainParagraph);
 
-// create aside
+// create aside - this will be parent for p, span
+let mountainAside = document.createElement("aside");
+article.append(mountainAside);
+
+// create children for `mountainAside`
+// create `mountainAside` paragraph
+let asideParagraph = document.createElement("p");
+mountainAside.append(asideParagraph);
+
+// create span element to go inside `asideParagraph`
+let asideSpan = document.createElement("span");
+asideSpan.textContent = " 4 Minutes";
+asideParagraph.append(asideSpan);
+
+// create `strong` element, to go inside `asideSpan`
+let spanStrong = document.createElement("strong");
+spanStrong.textContent = "Read Time:";
+asideSpan.prepend(spanStrong);
+
+// create `a` element, to go inside `asideSpan`
+let spanAnchor = document.createElement("a");
+spanAnchor.setAttribute("href", "#");
+spanAnchor.textContent = "Read more...";
+asideSpan.append(spanAnchor);
 
 
 
