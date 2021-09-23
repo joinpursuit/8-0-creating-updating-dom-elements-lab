b7750a3a5111b3c490118154f71cab25c1f1d30e
BennyZ3 <bennyz3@hotmail.com>
2021-09-23 13:50:18 -0400

    finished cypress tests

---
+++ main.js
@@ -0,0 +1,30 @@
+document.querySelector("section").classList.add("featured");
+
+const newArticle = document.createElement("article");
+const image = document.createElement("img");
+image.src = "./images/paul-gilmore-unsplash.jpg";
+image.alt = "Image of a mountain in front of a lake.";
+newArticle.append(image);
+const stopH3 = document.createElement("h3");
+stopH3.textContent = "Stop Planning";
+newArticle.append(stopH3);
+const planningP = document.createElement("p");
+planningP.textContent = `You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?`;
+newArticle.append(planningP);
+const aside = document.createElement("aside");
+const asideP = document.createElement("p");
+const asidePSpan = document.createElement("span");
+asidePSpan.innerHTML = "<strong>Read Time:</strong> 4 Minutes";
+asideP.append(asidePSpan);
+const asidePA = document.createElement("a");
+asidePA.href = "#";
+asidePA.textContent = "Read more...";
+asideP.append(asidePA);
+aside.append(asideP);
+newArticle.append(aside);
+const posts = document.querySelector(".posts");
+posts.append(newArticle);
+
+// console.log(document.querySelector(".posts"));
+const articles = document.querySelectorAll("article");
+articles[2].parentNode.insertBefore(articles[2], articles[1]);
