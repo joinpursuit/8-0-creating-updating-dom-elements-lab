e796b1cbb1647c775db97cf45622776f771b4b57
Fatema Kabir <fatema.kabir@pursuit.org>
2021-09-23 19:16:40 -0400

    added-class-featured

---
+++ main.js
@@ -0,0 +1,6 @@
+// const heading = document.querySelector("section");
+// heading.setAttribute("class", "featured");
+// .setAttribute method 
+
+const heading = document.querySelector('section');
+heading.classList.add('featured');
