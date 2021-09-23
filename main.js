142e0e20176e5ca8ed7c7b4158fee0db77e52ebb
caveman49 <caveman4949@gmail.com>
2021-09-23 14:26:39 -0400

    first_test_passed

---
+++ main.js
@@ -0,0 +1,5 @@
+// const heading = document.querySelector("section");
+// heading.setAttribute("class", "featured");
+
+const heading = document.querySelector('section');
+heading.classList.add('featured');
