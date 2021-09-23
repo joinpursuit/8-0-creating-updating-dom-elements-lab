39ee163e786176d950fcba91df7027b6314989e3
Younes Tahiri <you@example.com>
2021-09-23 16:06:16 -0400

    Completed the 8-0-creating-updating-dom-elements-lab

---
+++ main.js
@@ -0,0 +1,46 @@
+const section1 = document.querySelector("section");
+section1.classList.add("featured");
+
+const posts = document.querySelector(".posts");
+
+const article3 = document.createElement("article");
+
+const image = document.createElement("img");
+image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+image.setAttribute("alt", "Image of a mountain in front of a lake.");
+
+const Header3 = document.createElement("h3");
+Header3.textContent = "Stop Planning";
+
+const paragraph1 = document.createElement("p");
+paragraph1.textContent = `You -- yes you! You're an over-planner, I can tell. It's time to stop
+planning so much and instead focusing on relaxing. Taking a break at all
+is so stressful these days; why add to the stress by overworking yourself?`;
+
+const aside = document.createElement("aside");
+const paragraph2 = document.createElement("p");
+
+const span = document.createElement("span");
+span.textContent = " 4 Minutes";
+
+const strong = document.createElement("strong");
+strong.textContent = "Read Time:";
+
+const anchor = document.createElement("a");
+anchor.setAttribute("href", "#");
+anchor.textContent = "Read more...";
+
+posts.append(article3);
+article3.append(image);
+article3.append(Header3);
+article3.append(paragraph1);
+article3.append(aside);
+
+aside.append(paragraph2);
+paragraph2.prepend(span);
+paragraph2.append(anchor);
+span.prepend(strong);
+
+const article1 = document.querySelector(".posts article");
+
+article3.before(article1);
