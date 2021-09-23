3033f897498f2b4ac2532480c037338c8170521d
Laiba Sajid <sajid.laiba378@gmail.com>
2021-09-23 16:08:43 -0400

    first attempt

---
+++ script.js
@@ -0,0 +1,36 @@
+// [ ] Add a class of "featured" to the first section element on the page.
+let section = document.querySelector("section")
+section.classList.add("featured");
+
+
+// [ ] Create the following article element with JavaScript and add at the end of the section.posts element.
+
+let article = document.createElement("article");
+let img = document.createElement("img");
+let h3 = document.createElement("h3");
+let p = document.createElement("p");
+let aside = document.createElement("aside");
+
+
+
+/* <article>
+  <img
+    src="./images/paul-gilmore-unsplash.jpg"
+    alt="Image of a mountain in front of a lake."
+  />
+  <h3>Stop Planning</h3>
+  <p>
+    You -- yes you! You're an over-planner, I can tell. It's time to stop
+    planning so much and instead focusing on relaxing. Taking a break at all
+    is so stressful these days; why add to the stress by overworking yourself?
+  </p>
+  <aside>
+    <p>
+      <span><strong>Read Time:</strong> 4 Minutes</span
+      ><a href="#">Read more...</a>
+    </p>
+  </aside>
+</article> */
+
+
+
