49f4a81469bee3f77e6dcc9da2f2ed26835ef332
Deborah Campos <631care@gmail.com>
2021-09-24 10:57:11 -0400

    all tests pass review comments added

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
-let section = document.querySelector("section");
-section.classList.add("featured");
+let section = document.querySelector("section");// declaring a variable (point of reference) for the first section (child) element of main / there are 2 section elements (children) total 
+section.classList.add("featured");// adding a class of featured to first section element
 
-let posts = document.querySelector("section.posts");
-let article = document.createElement("article");
+let posts = document.querySelector("section.posts");// setting a variable (point of reference) for the 2nd section / child element of main
+let article = document.createElement("article");// creating an article element
+posts.append(article);//appending new article element to the end of the 2nd section element, grabbed by class of .posts
 
-posts.append(article);
-
-let image = document.createElement("img");
-image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+let image = document.createElement("img");// seeting a variable for image element
+image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");// setting attributes to img tag
 image.setAttribute("alt","Image of a mountain in front of a lake.");
-article.append(image);
+article.append(image);// appending image to article element.  img is now child of third article in .posts class
 
-let h3 = document.createElement("h3");
-h3.textContent = "Stop Planning";
-article.append(h3);
+let h3 = document.createElement("h3");// creating h3 element 
+h3.textContent = "Stop Planning";// setting text for new h3 elememt 
+article.append(h3); // appending h3 to article,  h3 is child of article, sibling of img element
 
-let paragraph = document.createElement("p");
+let paragraph = document.createElement("p");// p element created
 paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
-article.append(paragraph);
+article.append(paragraph);// p element now child of article, sibling of img & h3
 
-let aside = document.createElement("aside");
-article.append(aside);
+let aside = document.createElement("aside");// aside elememt created 
+article.append(aside);// aside set as child of article, sibling of h3, p, & img
 
-let newP = document.createElement("p")
-aside.append(newP);
-let span = document.createElement("span");
-newP.append(span);
-//span.textContent = "<span><strong>Read Time:</strong> 4 Minutes</span><a href="#">Read more...</a>"
+let newP = document.createElement("p")// another p element created
+aside.append(newP);// p element set as child of aside 
 
-// let asideP = document.querySelector("aside p")
+let span = document.createElement("span");//creating span elment 
+newP.append(span);// placing span as child of aside 
+span.textContent = " 4 Minutes"// setting text for span element
 
-// let span = document.createElement("span");
-span.textContent = " 4 Minutes"
-let strong = document.createElement("strong");
-strong.textContent = "Read Time"
-span.prepend(strong);
- let link = document.createElement("a");
-link.textContent = "Read more";
-link.setAttribute("href", "#");
-newP.append(link);
+let strong = document.createElement("strong");// creating strong element
+strong.textContent = "Read Time"// 
+span.prepend(strong);// setting strong element, child of span, with text before span text content
 
+let link = document.createElement("a");//anchor tag created
+link.textContent = "Read more";// setting text for anchor
+link.setAttribute("href", "#");// setting attribute for anchor tag
+newP.append(link);// anchor tag is now child of p element(child of aside), sibling of span element
 
+let articles = document.querySelectorAll("section.posts article");// grabbing all articles with class of .posts 
+console.log(articles[1]);// grabbing the second article, with intention of swapping with first article[0] element 
+posts.prepend(articles[1]);// swapping the place of the second article(child of section with class of .posts) with the first article element
 
 
