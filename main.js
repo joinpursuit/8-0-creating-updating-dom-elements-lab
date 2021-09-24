ca21d3aaedbf4ae87a09c7104100092a54b70f21
Lakisha J <lakishajohnson@pursuit.org>
2021-09-23 20:48:26 -0400

    Post complete

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const section = document.querySelector('section');
 section.classList.add('featured');
 
 const article = document.createElement('article');
-const image = document.createElement('img');
+document.querySelector('section.posts').append(article);
 
+const image = document.createElement('img');
 image.setAttribute('src',"./images/paul-gilmore-unsplash.jpg");
 image.setAttribute('alt', "Image of a mountain in front of a lake.");
 
 article.append(image);
-document.querySelector('section.posts').append(article);
 
 const head = document.createElement('h3');
 head.textContent = 'Stop Planning'
@@ -20,25 +20,41 @@ planning so much and instead focusing on relaxing. Taking a break at all
 is so stressful these days; why add to the stress by overworking yourself?`
 article.append(paragraph);
 
-const aside = document.createElement('aside')
 
-paragraph.after(aside);
+const aside = document.createElement('aside')
+article.append(aside);
 
 const paragraph2 = document.createElement('p');
-aside.prepend(paragraph2);
+aside.append(paragraph2);
+
 
 const span = document.createElement('span');
-paragraph2.prepend(span);
+span.textContent = '4 Minutes';
+paragraph2.append(span);
 
 const strong = document.createElement('strong');
-span.after(strong);
+strong.textContent = 'Read Time: ';
+span.before(strong);
 
-strong.textContent = 'Read Time:';
-span.textContent = '4 Minutes';
+//aside.prepend(paragraph2);
 
-//span.setAttribute('font-size: 14px');
+//span.prepend(strong);
+span.setAttribute('font-size', '14px');
 
-article.append(span);
+// article.append(span);
+
+const link = document.createElement('a');
+link.setAttribute('href', '#');
+link.textContent = 'Read more...';
+paragraph2.append(link);
+
+const articleSwap=
+
+
+
+
+
+
 
 
 
