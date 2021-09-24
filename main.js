8b499587eea403416f9729dfb67ac250bf4d0af9
imolina212 <86175612+imolina212@users.noreply.github.com>
2021-09-23 20:48:29 -0400

    post complete

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 //added class 'featured'
 const section = document.querySelector("section");
 section.classList.add("featured");
-//created new article and img tag
+//created new article
 const article = document.createElement("article");
+//create article to section.post
+document.querySelector("section.posts").append(article);
+//create img tag
 const image = document.createElement("img");
 //add attributes to img
 image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
 image.setAttribute("alt", "Image of a mountain in front of a lake.");
 //add img to article
 article.append(image);
-//create article to section.post
-document.querySelector("section.posts").append(article);
 //create heading
 const head = document.createElement("h3");
 head.textContent = "Stop Planning";
@@ -23,22 +24,25 @@ is so stressful these days; why add to the stress by overworking yourself?`;
 article.append(paragraph);
 //create aside tag and add to aside
 const aside = document.createElement("aside");
-paragraph.after(aside);
-//create strong tag
-const strong = document.createElement("strong");
-//create span tag and add to p2
-const span = document.createElement("span");
+article.append(aside);
 //create 2nd paragraph
 const paragraph2 = document.createElement("p");
-paragraph2.prepend(span);
-//add p2 to aside tag
-aside.prepend(paragraph2);
-//add span to p2
-paragraph2.prepend(span);
-//add text to span tag
+aside.append(paragraph2);
+//create span tag and add to p2
+const span = document.createElement("span");
 span.textContent = "4 Minutes";
-strong.textContent = "Read time: ";
-span.prepend(strong);
+//create strong tag
+paragraph2.append(span);
+const strong = document.createElement("strong");
+strong.textContent = "Read Time: ";
+//add p2 to aside tag
+span.before(strong);
 //add style
-span.setAttribute("font-size: 14px");
-article.append(span);
+span.setAttribute("font-size:", "14px");
+// article.append(span);
+//create a tag
+const link = document.createElement("a");
+//add href attribute
+link.setAttribute("href", "#");
+link.textContent = "Read more...";
+paragraph2.append(link);
