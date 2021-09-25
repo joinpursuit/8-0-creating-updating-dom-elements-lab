b69512e92609edbf3492c6ec09d69b31327638f1
Laiba Sajid <sajid.laiba378@gmail.com>
2021-09-24 22:32:58 -0400

    completed first test

--- script.js
+++ script.js
@@ -26,12 +26,12 @@ let p2 = document.createElement("p");
 let span = document.createElement("span"); //not sure how to put strong tags 
 let a = document.createElement("a");
 let bold = document.createElement("strong")
-span.textContent = "4 minutes"
+span.textContent = " 4 Minutes"
 bold.textContent = "Read Time:"
 a.setAttribute("href", "#"); // how do you ass the 
 a.textContent = "Read more..."
 
-sandwhich.after(article)
+sandwhich.append(article)
 article.prepend(img);
 article.append(h3);
 article.append(p1);
@@ -40,27 +40,17 @@ article.append(aside);
 aside.prepend(p2)
 span.prepend(bold)
 p2.prepend(span);
-p2.prepend(a);
+p2.append(a);
 
 
-/* <article>
-  <img
-    src="./images/paul-gilmore-unsplash.jpg"
-    alt="Image of a mountain in front of a lake."
-  />
-  <h3>Stop Planning</h3>
-  <p>
-    You -- yes you! You're an over-planner, I can tell. It's time to stop
-    planning so much and instead focusing on relaxing. Taking a break at all
-    is so stressful these days; why add to the stress by overworking yourself?
-  </p>
-  <aside>
-    <p>
-      <span><strong>Read Time:</strong> 4 Minutes</span
-      ><a href="#">Read more...</a>
-    </p>
-  </aside>
-</article> */
+// [ ] Switch the first and Second Articles 
+gamesArticle = document.querySelector("section.posts")
+// how do you grab the second architectureArticle = document.querySelector()
 
+posts = document.querySelectorAll("section.posts")
 
-
+// for(post of posts){
+//   if(post.name = Games to play){
+      // insertNode Before ()
+// }
+// }
