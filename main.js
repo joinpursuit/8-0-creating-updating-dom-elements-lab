d7dd60996993d1b27d269f70a377f4d7030e3c06
theodorefrazier <theo.frazier@icloud.com>
2021-09-26 16:54:29 -0400

    some typos in strong were making the test not run. I fixed those and now the test run - all text running except STRONG - even though the 'read more' text is bolding in the website in Cypress test

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ img.setAttribute("alt", "Image of a mountain in front of a lake."); /* setting a
 /* ------------- HEADING 3 ------------------------------ */
 
 let heading3 = document.createElement("h3"); /* ------- setting h3 ---------- */
-heading3.textContent = "hi";
+heading3.textContent = "Stop Planning";
 
 /* ------------------ PARAGRAPH ---------------------------------- */
 
@@ -34,3 +34,50 @@ article.append(img);
 article.append(heading3);
 article.append(articleParagaph);
 
+/* -------------- Building the Aside Element ----------------- */
+
+let aside = document.createElement("aside");
+let asideParagaph = document.createElement("p");
+let span = document.createElement("span");
+let strong = document.createElement("strong");
+let anchorTag = document.createElement("a");
+
+/* ------------ assigning ----------------- */
+
+span.textContent = " 4 Minutes";
+strong.textContent = "Read Time: ";
+anchorTag.setAttribute("href", "#");
+anchorTag.textContent = "Read more...";
+
+/* ------------- adding the elements to each other  -------------  */
+
+
+/* aside assigns to article */
+article.append(aside)
+/* paragraph to aside */
+aside.append(asideParagaph)
+/* span to aside paragraph */
+aside.append(span)
+/* Span before strong */
+span.prepend(strong);
+/* anchor tag goes to the paragraph */
+asideParagaph.append(anchorTag)
+
+/* ----- node list fucntions like an array, first aritcle is at "index" zero ------- */
+
+let postsInSection = document.getElementsByClassName("posts")[0];
+
+/*----- moves first article to the end of node list ------------  */
+
+postsInSection.appendChild(article);
+
+/* ------- makes the first post became the first element child ------- */
+
+let firstPost = postsInSection.firstElementChild;
+
+
+/* --------- makes the second post the next element in the node list ------- */
+
+let secondPost = firstPost.nextElementSibling;
+
+postsInSection.insertBefore(secondPost, firstPost)
