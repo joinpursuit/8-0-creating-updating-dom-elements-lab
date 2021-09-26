6751ec58fc87c90ecb96755c8c22dd4d05c12e6c
theodorefrazier <theo.frazier@icloud.com>
2021-09-26 15:16:50 -0400

    forgot to link script file - now that it is added one test is passing

---
+++ main.js
@@ -0,0 +1,30 @@
+/* Since querySelector always selctions first - use querySelector  */
+
+let section = document.querySelector("section");
+
+/* classList will let me add an class */
+
+section.classList.add("featured"); /* adds featued */
+
+/* ------------ CREATING THE ARTICLE ELEMENT ---------------- */
+
+let article = document.createElement("article"); /* create the article */
+let img = document.createElement("img") /* creating img element */
+
+/* ------------- SETTING THE IMAGE --------------------- */
+
+img.setAttribute("src", "./images/paul-gilmore-unsplash.jpg"); /* image */
+img.setAttribute("alt", "Image of a mountain in front of a lake."); /* setting alt */
+
+/* ------------- HEADING 3 ------------------------------ */
+
+let heading3 = document.createElement("h3"); /* ------- setting h3 ---------- */
+heading3.textContent = "hi";
+
+/* ------------------ PARAGRAPH ---------------------------------- */
+
+let articleParagaph = document.createElement("p") /* -------- creating paragraph ---------- */
+articleParagaph.textContent =  `You -- yes you! You're an over-planner, I can tell. It's time to stop
+planning so much and instead focusing on relaxing. Taking a break at all
+is so stressful these days; why add to the stress by overworking yourself`
+
--- readme.md
+++ readme.md
@@ -38,8 +38,8 @@ To complete the tests in this lab, you will need to create and update a number o
 
 If you're unclear about any of the instructions below, remember that you can check the test file to see what exactly is being tested.
 
-- [ ] Add a class of "featured" to the first `section` element on the page.
-- [ ] Create the following `article` element with JavaScript and add at the end of the `section.posts` element.
+- [✅ ] Add a class of "featured" to the first `section` element on the page.
+- [✅ ] Create the following `article` element with JavaScript and add at the end of the `section.posts` element.
   ```html
   <article>
     <img
