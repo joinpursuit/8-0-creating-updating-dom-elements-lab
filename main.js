589e5057ed34f0ef1b57f0290839d6cfc543c3fc
Edwin Codrington <edwincodrington@gmail.com>
2022-03-29 01:07:41 -0400

    Completed lab

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-const firstSection = document.getElementById("section");
-//firstSection.classList.toggle("featured");
+const firstSection = document.querySelector("section");
+firstSection.classList.toggle("featured"); //causes error?!
 
 const newArticle = document.createElement("article");
+document.querySelector("section.posts").append(newArticle);
 
 const newImg = document.createElement("img");
+newArticle.append(newImg);
 newImg.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
 newImg.setAttribute("alt", "Image of a mountain in front of a lake.");
-newArticle.append(newImg);
 
 const newH3 = document.createElement("h3");
-newH3.textContent = "Stop Planning";
 newArticle.append(newH3);
+newH3.textContent = "Stop Planning";
 
 const newParagraph = document.createElement("p");
+newArticle.append(newParagraph);
 newParagraph.textContent = `    You -- yes you! You're an over-planner, I can tell. It's time to stop
     planning so much and instead focusing on relaxing. Taking a break at all
     is so stressful these days; why add to the stress by overworking yourself?`;
-
-newArticle.append(newParagraph);
 
 //create aside
 const aside = document.createElement("aside");
@@ -41,15 +41,5 @@ aside.append(p);
 //attach aside to article
 newArticle.append(aside);
 //attach article to end of section.posts
-const sections = document.querySelectorAll("section");
-for (let section of sections) {
-	if (section.classList.contains("posts")) {
-		//swap 1 and 2
-		let temp;
-		let article1 = section.querySelector("article");
-		temp = article1.innerHTML;
-		article1.remove(); //delete 1, #2 is now in front
-		section.append(temp); //re-add #1 to position 2
-		section.append(newArticle); // add new article to the end
-	}
-}
+const sections = document.querySelectorAll("section.posts article");
+sections[1].after(sections[0]);
