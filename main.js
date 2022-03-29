432fbffaaf985ab53393827bfb93e86cd7d92398
named.josie <named.josie@gmail.com>
2022-03-28 22:43:23 -0400

    created article

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 let firstSection = document.querySelector('section');
 firstSection.setAttribute('class', 'featured');
 
+let article = document.createElement('article');
+document.querySelector('section.posts').append(article);
+
