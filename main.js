f3e3b5c95c912b3b8424a0923202cfbda92012c4
named.josie <named.josie@gmail.com>
2022-03-28 23:43:10 -0400

    finished Updating-dom lab

--- main.js
+++ main.js
@@ -4,3 +4,35 @@ firstSection.setAttribute('class', 'featured');
 let article = document.createElement('article');
 document.querySelector('section.posts').append(article);
 
+let img = document.createElement('img');
+img.setAttribute('src', './images/paul-gilmore-unsplash.jpg');
+img.setAttribute('alt', 'Image of a mountain in front of a lake.');
+
+let h3 = document.createElement('h3');
+h3.textContent = 'Stop Planning';
+
+let p1 = document.createElement('p');
+p1.textContent =
+  `You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?`;
+
+  let aside = document.createElement('aside');
+  article.append(img, h3, p1, aside);
+  
+  let p2 = document.createElement('p');
+  aside.append(p2);
+  
+  let span = document.createElement('span');
+  p2.append(span);
+  span.textContent = '4 Minutes';
+  
+  let strong = document.createElement('strong');
+  span.prepend(strong);
+  strong.textContent = 'Read Time:';
+  
+  let a = document.createElement('a');
+  span.after(a);
+  a.textContent = 'Read more...';
+  a.setAttribute('href', '#');
+  
+  let switchArticlePosition = document.querySelectorAll('section.posts article');
+  switchArticlePosition[1].after(switchArticlePosition[0]);
