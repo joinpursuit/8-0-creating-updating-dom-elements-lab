bdf871e6e5019bf8fa6d0f4a1e0e5a30cec95781
Trevor Triumph <jumanji16y@aol.com>
2022-03-29 02:15:45 -0400

    final update on lab

--- main.js
+++ main.js
@@ -5,20 +5,20 @@ let article = document.createElement("article");
 let sectionPosts = document.querySelector("section.posts");
 sectionPosts.append(article);
 
-let image = document.createElement("img");
-image.setAttribute("src","./images/paul-gilmore-unsplash.jpg");
-image.setAttribute("alt", "Image of a mountain in front of a lake.");
-article.append(image);
+let img = document.createElement("img");
+img.setAttribute("src","./images/paul-gilmore-unsplash.jpg");
+img.setAttribute("alt", "Image of a mountain in front of a lake.");
+article.append(img);
 
-let head = document.createElement('h3');
-head.textContent = "Stop Planning";
-article.append(head);
+let h3 = document.createElement('h3');
+h3.textContent = "Stop Planning";
+article.append(h3);
 
-let paragraph = document.createElement("p");
-paragraph.textContent = `You -- yes you! You're an over-planner, I can tell. It's time to stop
+let p = document.createElement("p");
+p.textContent = `You -- yes you! You're an over-planner, I can tell. It's time to stop
 planning so much and instead focusing on relaxing. Taking a break at all
 is so stressful these days; why add to the stress by overworking yourself?`
-article.append(paragraph);
+article.append(p);
 
 let aside = document.createElement("aside")
 article.append(aside);
