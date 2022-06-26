59c663331076c7d2c33c6cc857b43da15186372d
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2022-06-26 15:35:11 -0400

    add main.js.

---
+++ main.js
@@ -0,0 +1,7 @@
+const section = document.createElement('section');
+const main = document.querySelector('main');
+main.prepend(section);
+section.classList.add('featured');
+
+const article = document.createElement('article');
+ 
