15208d18c594ca693dfd0aa226e851a0a3b0328c
Amina-Moufakkir <aminamoufakkir@pursuit.org>
2022-08-22 13:17:40 -0400

    creating-updating-dom-elements completed

---
+++ main.js
@@ -0,0 +1,41 @@
+const section = document.querySelector("section").setAttribute("class", "featured");
+
+let article = document.createElement("article");
+document.querySelector("section.posts").append(article);
+
+
+let img = document.createElement("img");
+img.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+img.setAttribute("alt", "Image of a mountain in front of a lake.");
+article.append(img);
+
+let h3 = document.createElement("h3");
+h3.textContent = "Stop Planning";
+article.append(h3);
+
+let p = document.createElement("p");
+p.textContent =
+  "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?";
+article.append(p);
+
+let aside = document.createElement("aside");
+article.append(aside);
+
+let paragraph = document.createElement("p");
+aside.append(paragraph);
+
+let span = document.createElement("span");
+span.textContent = " 4 Minutes";
+paragraph.append(span);
+
+let strong = document.createElement("strong");
+strong.textContent = "Read Time:";
+span.prepend(strong);
+
+let a = document.createElement("a");
+a.textContent = "Read more";
+a.setAttribute("href", "#");
+span.after(a);
+
+let articles = document.querySelectorAll("section.posts article");
+articles[1].after(articles[0])
