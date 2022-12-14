3a68d3be975d59e9d723f14e89ec726c61253d5a
ariunaamy <ariun.myagmar@gmail.com>
2022-12-14 14:07:17 -0500

    complete final

--- main.js
+++ main.js
@@ -8,33 +8,32 @@ sectionPosts.append(article);
 const articles = document.querySelectorAll(".posts article");
 articles[0].before(articles[1]);
 
-const image = document.createElement("img")
-articles[2].append(image)
+const image = document.createElement("img");
+articles[2].append(image);
 image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
 image.setAttribute("alt", "Image of a mountain in front of a lake.");
 
-const h3 = document.createElement("h3")
-articles[2].append(h3)
+const h3 = document.createElement("h3");
+articles[2].append(h3);
 h3.innerText = "Stop Planning";
 
-const p = document.createElement("p")
-articles[2].append(p)
-p.innerText = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+const p = document.createElement("p");
+articles[2].append(p);
+p.innerText = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?";
 
-const aside = document.createElement("aside")
-articles[2].append(aside)
+const aside = document.createElement("aside");
+articles[2].append(aside);
 
 const p2 = document.createElement("p");
-aside.append(p2)
+aside.append(p2);
 
-const span = document.createElement("span")
-const strong = document.createElement("strong")
+const span = document.createElement("span");
+const strong = document.createElement("strong");
 
-p2.append(strong)
-p2.append(span)
-
-strong.innerText = "Read Time: "
-span.innerText = "4 Minutes"
+p2.append(span);
+strong.innerText = "Read Time:";
+span.innerText = " 4 Minutes";
+span.prepend(strong);
 
 const a = document.createElement("a");
 a.setAttribute("href", "#")
