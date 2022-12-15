52007fd1cae81d14d5679c188169159b4577f9a7
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-15 04:12:19 -0500

    Testcase 2 passed

--- main.js
+++ main.js
@@ -1,2 +1,46 @@
-let searchSection = document.querySelector("section")
-searchSection.classList.add("featured");
+const section = document.querySelector("section")
+section.classList.add("featured");
+
+const newArticle = document.createElement("article");
+const post = document.querySelector("section.posts");
+post.append(newArticle);
+
+const image = document.createElement("img")
+newArticle.prepend(image)
+image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
+image.setAttribute("alt", "Image of a mountain in front of a lake.");
+
+const heading = document.createElement("h3")
+newArticle.append(heading)
+heading.textContent = 'Stop Planning'
+
+const paragraph = document.createElement("p")
+newArticle.append(paragraph)
+paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+
+const aside = document.createElement("aside")
+newArticle.append(aside)
+
+const p = document.createElement("p")
+aside.append(p)
+
+const span = document.createElement("span")
+p.append(span)
+
+const strong = document.createElement("strong")
+span.append(strong)
+strong.textContent = "Read Time:"
+span.textContent = "4 Minutes"
+
+const a = document.createElement("a")
+p.append(a)
+
+a.setAttribute("href","#")
+a.textContent = "Read more..."
+
+/* <aside>
+      <p>
+        <span><strong>Read Time:</strong> 4 Minutes</span
+        ><a href="#">Read more...</a>
+      </p>
+    </aside> */
