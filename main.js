1f67b68b12c6a444dab1e64791eeee5ab9f562d2
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-12-15 05:04:57 -0500

    Comments Added

--- main.js
+++ main.js
@@ -6,44 +6,44 @@ section.classList.add("featured");
 
 // testcase 2
 
-// create new article and at to end (append) of section.posts
+// create new article and add to end (append) of section.posts
 const newArticle = document.createElement("article");
-const post = document.querySelector("section.posts");
-post.append(newArticle);
+const postSection= document.querySelector("section.posts");
+postSection.append(newArticle);
 
 // create new image and at to the top (prepend) of article
 const image = document.createElement("img")
 newArticle.prepend(image)
-
 // setting image attributes
 image.setAttribute("src", "./images/paul-gilmore-unsplash.jpg");
 image.setAttribute("alt", "Image of a mountain in front of a lake.");
 
+
 // create new h3 element and append to article
 const heading = document.createElement("h3")
 newArticle.append(heading)
-
 // add content to h3
 heading.textContent = 'Stop Planning'
 
-// create new paragraph element and append to article
-const paragraph = document.createElement("p")
-newArticle.append(paragraph)
 
+// create new paragraph element and append to article
+const paragraphArticle = document.createElement("p")
+newArticle.append(paragraphArticle)
 // add content to paragraph
-paragraph.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+paragraphArticle.textContent = "You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?"
+
 
 // create aside tag and append to article
 const aside = document.createElement("aside")
 newArticle.append(aside)
 
 // create a paragraph element inside aside element.
-const p = document.createElement("p")
-aside.append(p)
+const paragraphAside = document.createElement("p")
+aside.append(paragraphAside)
 
 // create span inside p and append to p
 const span = document.createElement("span")
-p.append(span)
+paragraphAside.append(span)
 
 // create strong inside span 
 const strong = document.createElement("strong")
@@ -51,12 +51,12 @@ span.append(strong)
 strong.textContent = "Read Time:"
 span.append("4 Minutes")    // we append else everything turns bold
 
-const a = document.createElement("a")
-span.after(a)
+const alink = document.createElement("a")
+span.after(alink)
 
-a.setAttribute("href","#")
-a.textContent = "Read more..."
+alink.setAttribute("href","#")
+alink.textContent = "Read more..."
 
 // testcase 3
 const articleArray = document.querySelectorAll("section.posts article")
-post.prepend(articleArray[1])
+postSection.prepend(articleArray[1])
