42142bcfcfe2631b239d2acaa29eb00d6381be15
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2022-12-15 14:21:28 -0500

    Pass first test

---
+++ main.js
@@ -0,0 +1,19 @@
+/* <-----------------1-------------------> */ 
+let featuredSec = document.querySelector("section")
+
+featuredSec.setAttribute("class", "featured")
+
+/* <-----------------Created Elem-------------------> */ 
+let sectionPosts = document.querySelector("section.posts")
+
+let article = document.createElement("article")
+
+let img = document.createElement("img")
+
+let heading = document.createElement("h3")
+let paragraph = document.createElement("img")
+let aside = document.createElement("img")
+let readTimeP = document.createElement("img")
+let span = document.createElement("img")
+let strong = document.createElement("img")
+let a = document.createElement("img")
