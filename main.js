aeb4506a0a5b0b16d5b79e40de53214a62ffc510
Fernando Cortez <cortez.fernando11216@gmail.com>
2022-12-15 00:17:37 -0500

    changed the variable name to be easier to read

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
-const js = document.querySelector("section") 
-js.setAttribute("class", "featured")
-const jss = document.querySelector("section.posts")
-const dinoTail = document.createElement("article")
-jss.append(dinoTail) 
-//console.log(jss)
-const dinoBody = document.createElement("img")
-dinoTail.append(dinoBody)
-//console.log(jss)
-dinoBody.setAttribute("src", "./images/paul-gilmore-unsplash.jpg")
-dinoBody.setAttribute("alt", "Image of a mountain in front of a lake.")
-const dinoScales = document.createElement("h3")
-dinoScales.innerText = "Stop Planning"
-dinoTail.append(dinoScales)
-const dinoTooth = document.createElement("p")
-dinoTooth.innerText = ("You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?")
-dinoTail.append(dinoTooth) 
-const dinoToes = document.createElement("aside")
-dinoTooth.append(dinoToes)
-const dinoNail = document.createElement("p")
-dinoToes.append(dinoNail) 
-const dinoHeart = document.createElement("span")
-dinoHeart.innerText = "4 Minutes" 
-dinoNail.append(dinoHeart) 
-const dinoBrain = document.createElement("strong")
-dinoBrain.innerText = "Read Time"
-dinoNail.append(dinoBrain) 
-const dinoWing = document.createElement("a")
-dinoWing.innerText = "Read more"
-dinoWing.setAttribute("href", "#")
-dinoNail.append(dinoWing)
-const firstDinoArticle = document.querySelector('.art1')
-const middleDinoArticle = document.querySelector('.art2')
-firstDinoArticle.before(middleDinoArticle)
+const section1 = document.querySelector("section") 
+section1.setAttribute("class", "featured")
+
+const post1 = document.querySelector("section.posts")
+
+const newPost1 = document.createElement("article")
+post1.append(newPost1) 
+
+
+const newImg1 = document.createElement("img")
+newPost1.append(newImg1)
+
+newImg1.setAttribute("src", "./images/paul-gilmore-unsplash.jpg")
+newImg1.setAttribute("alt", "Image of a mountain in front of a lake.")
+
+const newh3 = document.createElement("h3")
+newh3.innerText = "Stop Planning"
+newPost1.append(newh3)
+
+const P1 = document.createElement("p")
+P1.innerText = ("You -- yes you! You're an over-planner, I can tell. It's time to stop planning so much and instead focusing on relaxing. Taking a break at all is so stressful these days; why add to the stress by overworking yourself?")
+newPost1.append(P1) 
+
+const standAside = document.createElement("aside")
+newPost1.append(standAside)
+
+const NewP = document.createElement("p")
+standAside.append(NewP) 
+
+const NewSpan = document.createElement("span")
+NewSpan.innerText = "4 Minutes" 
+NewP.append(NewSpan) 
+
+const GetStrong = document.createElement("strong")
+GetStrong.innerText = "Read Time"
+NewP.append(GetStrong) 
+
+const Atag = document.createElement("a")
+Atag.innerText = "Read more"
+Atag.setAttribute("href", "#")
+NewP.append(Atag)
+
+const firstArticle = document.querySelector('.art1')
+const middleArticle = document.querySelector('.art2')
+firstArticle.before(middleArticle)
